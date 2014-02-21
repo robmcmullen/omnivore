20f0c830ce0f1c6ae1dfee1988a9f088786f4ca7
Rob McMullen <robm@users.sourceforge.net>
2014-02-20 21:49:41 -0800

    Added py2exe utility to find wx modules dynamically imported

--- peppy2/__init__.py
+++ peppy2/__init__.py
@@ -28,3 +28,33 @@ except ImportError:
     __version__ = "0.1.dev"
     __codename__ = "git-codename"
     __revision__ = "HEAD"
+
+
+# py2exe utilities
+def get_py2exe_toolkit_includes(module=None, toolkit="wx"):
+    """Get a list of modules that should be included in a py2exe/py2app
+    distribution but because they are imported dynamically must be explicitly
+    included.
+    
+    This list of files should be added to options keyword parameter of the
+    setup call; e.g:
+    
+    setup(options={"py2exe": {"includes": peppy2.get_py2exe_toolkit_includes()}})
+    """
+    if module is None:
+        path = __file__
+    else:
+        path = module.__file__
+    import os
+    basedir, module = os.path.split(os.path.dirname(path))
+    #print basedir, module
+    suffix = "_%s.py" % toolkit
+    includes = []
+    for root, dirs, files in os.walk(os.path.join(basedir, module)):
+        #print root, files
+        mod_root = root[len(basedir) + 1:]
+        needed = [f for f in files if f.endswith(suffix)]
+        for f in needed:
+            mod_name = ("%s.%s" % (mod_root, f[:-3])).replace("/", ".")
+            includes.append(mod_name)
+    return includes
