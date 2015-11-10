5bd34bad56d98c4e4b72c48a5dde4fa916f23ac6
Rob McMullen <feedback@playermissile.com>
2015-11-10 15:45:48 -0800

    Changed to palette map instead of toggle buttons

--- omnimon/utils/wx/antic_colors.py
+++ omnimon/utils/wx/antic_colors.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 import wx
 import wx.lib.buttons as buttons
 import wx.lib.sized_controls as sc
 import wx.lib.colourselect as csel
+import wx.lib.colourchooser.canvas as canvas
 
 from omnimon.utils import colors
 
@@ -35,7 +38,6 @@ class ColorListBox(wx.VListBox):
         dc.DrawLabel(label, rect, wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL)
         w, h = self.GetTextExtent(label)
         color = self.GetParent().get_color(n)
-        print color
         dc.SetBrush(wx.Brush(color))
         rect.OffsetXY(w, 0)
         rect.Deflate(2, 2)
@@ -49,44 +51,110 @@ class ColorListBox(wx.VListBox):
         return h + 5
 
 
+class AnticPalette(canvas.Canvas):
+    """Large image selection using Antic colors
+    """
+
+    HORIZONTAL_STEP = 24
+    VERTICAL_STEP = 24
+    BORDER = 4
+    HIGHLIGHT_WIDTH = 5
+
+    def __init__(self, parent, editor):
+        """Creates a palette object."""
+        # Load the pre-generated palette XPM
+        
+        # Leaving this in causes warning messages in some cases.
+        # It is the responsibility of the app to init the image
+        # handlers, IAW RD
+        #wx.InitAllImageHandlers()
+        
+        self.palette = self.init_palette(editor)
+        print "palette size", self.palette.GetSize()
+        canvas.Canvas.__init__(self, parent, -1, size=self.palette.GetSize(), style=wx.SIMPLE_BORDER)
+
+    def init_palette(self, editor):
+        w = self.HORIZONTAL_STEP * 16 + 2 * self.BORDER
+        h = self.VERTICAL_STEP * 16 + 2 * self.BORDER
+        array = np.empty((h, w, 3), dtype=np.uint8)
+        array[:,:] = editor.empty_color
+        for high in range(0, 256, 16):
+            y = self.BORDER + (high / 16) * self.VERTICAL_STEP
+            for low in range(16):
+                x = self.BORDER + low * self.HORIZONTAL_STEP
+                c = colors.gtia_ntsc_to_rgb(high + low)
+                array[y:y+self.VERTICAL_STEP,x:x+self.HORIZONTAL_STEP,:] = c
+        width = array.shape[1]
+        height = array.shape[0]
+        image = wx.EmptyImage(width, height)
+        image.SetData(array.tostring())
+        bmp = wx.BitmapFromImage(image)
+        return bmp
+
+    def DrawBuffer(self):
+        """Draws the palette XPM into the memory buffer."""
+        #self.GeneratePaletteBMP ("foo.bmp")
+        self.buffer.DrawBitmap(self.palette, 0, 0, 0)
+
+    def highlight_xy(self, x, y):
+        """Highlights an area of the palette with a rectangle around
+        the coordinate point"""
+        low = (x - self.BORDER) // self.HORIZONTAL_STEP
+        high = (y - self.BORDER) // self.VERTICAL_STEP
+        return self.highlight(high, low)
+
+    def highlight_color(self, c):
+        high, low = divmod(c, 16)
+        return self.highlight(high, low)
+    
+    def highlight(self, high, low):
+        colour = wx.SystemSettings.GetColour(wx.SYS_COLOUR_HIGHLIGHTTEXT)
+        self.buffer.SetPen(wx.Pen(colour, self.HIGHLIGHT_WIDTH, wx.SOLID))
+        self.buffer.SetBrush(wx.Brush(colour, wx.TRANSPARENT))
+        low = 15 if low > 15 else low
+        low = 0 if low < 0 else low
+        high = 15 if high > 15 else high
+        high = 0 if high < 0 else high
+        x = self.BORDER + low * self.HORIZONTAL_STEP
+        y = self.BORDER + high * self.VERTICAL_STEP
+        self.buffer.DrawRectangle(x, y, self.HORIZONTAL_STEP, self.VERTICAL_STEP)
+        self.Refresh()
+        return high * 16 + low
+
 class AnticColorDialog(wx.Dialog):
     def __init__(self, parent, editor):
         wx.Dialog.__init__(self, parent, -1, "Choose ANTIC Colors")
         self.editor = editor
         self.num_cols = 17
 
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        self.name = wx.StaticText(self, -1, "Editing Color")
+        sizer.Add(self.name, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+
         box = wx.BoxSizer(wx.HORIZONTAL)
         self.color_registers = ColorListBox(self, -1, style=wx.VSCROLL, size=(100, -1))
-        box.Add(self.color_registers, 0, wx.EXPAND, 0)
-        self.grid = wx.FlexGridSizer(cols=self.num_cols, hgap=2, vgap=2)
-        box.Add(self.grid, 1, wx.EXPAND, 0)
-
-        self.init_grid()
-        self.init_colors()
-
-        vsiz = wx.BoxSizer(wx.VERTICAL)
-        self.name = wx.StaticText(self, -1, "Editing Color")
-        vsiz.Add(self.name, 0, wx.EXPAND)
-        vsiz.Add(box, 1, wx.EXPAND)
-#        btn_sizer = self.CreateStdDialogButtonSizer(wx.OK | wx.CANCEL)
-#        vsiz.Add(btn_sizer, 0, wx.EXPAND, border=12)
-        btnsizer = wx.StdDialogButtonSizer()
-        btn = wx.Button(self, wx.ID_OK)
-        btn.SetDefault()
-        btnsizer.AddButton(btn)
-        btn = wx.Button(self, wx.ID_CANCEL)
-        btnsizer.AddButton(btn)
-        btnsizer.Realize()
-        vsiz.Add(btnsizer, 0, wx.EXPAND)
+        box.Add(self.color_registers, 0, wx.EXPAND|wx.ALL, 4)
+        self.palette = AnticPalette(self, editor)
+        box.Add(self.palette, 1, wx.ALL, 4)
+        sizer.Add(box, 1, wx.GROW|wx.ALL, 5)
+        
+        btn_sizer = self.CreateStdDialogButtonSizer(wx.OK | wx.CANCEL)
+        sizer.Add(btn_sizer, 0, wx.GROW|wx.ALL, 12)
 
         self.Bind(wx.EVT_CLOSE, self.on_close)
         self.Bind(wx.EVT_BUTTON, self.on_button)
         self.Bind(wx.EVT_LISTBOX, self.on_set_register)
-        self.Bind(wx.EVT_TOGGLEBUTTON, self.on_set_color)
+        self.mouse_down = False
+        self.palette.Bind(wx.EVT_LEFT_DOWN, self.onPaletteDown)
+        self.palette.Bind(wx.EVT_LEFT_UP, self.onPaletteUp)
+        self.palette.Bind(wx.EVT_MOTION, self.onPaletteMotion)
 
-        border = wx.BoxSizer(wx.VERTICAL)
-        border.Add(vsiz, 0, wx.ALL, 25)
-        self.SetSizer(border)
+        self.init_colors()
+
+#        border = wx.BoxSizer(wx.VERTICAL)
+#        border.Add(sizer, 0, wx.GROW|wx.ALL, 25)
+        self.SetSizer(sizer)
         self.Layout()
         self.Fit()
     
@@ -104,59 +172,6 @@ class AnticColorDialog(wx.Dialog):
     def get_color(self, register):
         return colors.gtia_ntsc_to_rgb(self.colors[register])
 
-    def init_grid(self):
-        self.grid.Clear(True)
-        b = wx.StaticText(self, -1, "")
-        self.grid.Add(b, flag=wx.ALIGN_CENTER_VERTICAL)
-        self.toggles = {}
-        for low in range(16):
-            b = wx.StaticText(self, -1, "%X" % low)
-            self.grid.Add(b, flag=wx.ALIGN_CENTER)
-        for high in range(0, 256, 16):
-            b = wx.StaticText(self, -1, "%X" % (high / 16))
-            self.grid.Add(b, flag=wx.ALIGN_CENTER_VERTICAL)
-            for low in range(16):
-                c = high + low
-                id = wx.NewId()
-                b = wx.ToggleButton(self, id, "", size=(24, 24))
-                self.toggles[id] = b, c
-                b.SetBitmap(self.make_bitmap(colors.gtia_ntsc_to_rgb(c)))
-                #b = buttons.GenBitmapToggleButton(self, -1, None, size=(32, 32))
-                #b.SetBitmapLabel(self.make_bitmap(colors.gtia_ntsc_to_rgb(c)))
-                self.grid.Add(b, flag=wx.ALIGN_CENTER_VERTICAL)
-        b = wx.StaticText(self, -1, "")
-        self.grid.Add(b, flag=wx.ALIGN_CENTER_VERTICAL)
-
-    def make_bitmap(self, color):
-        """From wx.lib.colourselect
-        
-        """
-        bdr = 12
-        width, height = self.GetSize()
-
-        # yes, this is weird, but it appears to work around a bug in wxMac
-        if "wxMac" in wx.PlatformInfo and width == height:
-            height -= 1
-            
-        bmp = wx.EmptyBitmap(width-bdr, height-bdr)
-        dc = wx.MemoryDC()
-        dc.SelectObject(bmp)
-        # Just make a little colored bitmap
-        #dc.SetBackground(wx.Brush(color))
-        dc.Clear()
-        dc.SetBrush(wx.Brush(color))
-        dc.DrawRectangle(10, 10, width, height)
-        dc.SelectObject(wx.NullBitmap)
-        return bmp
-    
-    def set_toggle(self, color):
-        for id, (b, c) in self.toggles.iteritems():
-            if c == color:
-                state = True
-            else:
-                state = False
-            b.SetValue(state)
-
     def on_button(self, event):
         if event.GetId() == wx.ID_OK:
             self.EndModal(wx.ID_OK)
@@ -173,11 +188,38 @@ class AnticColorDialog(wx.Dialog):
         self.current_register = event.GetSelection()
         print "register", self.current_register
         color = self.colors[self.current_register]
-        self.set_toggle(color)
+        self.palette.ReDraw()
+        self.palette.highlight_color(color)
     
     def on_set_color(self, event):
         id = event.GetId()
         b, c = self.toggles[id]
+        self.colors[self.current_register] = c
+        self.color_registers.Refresh()
+
+    def onPaletteDown(self, event):
+        """Stores state that the mouse has been pressed and updates
+        the selected colour values."""
+        self.mouse_down = True
+        self.palette.ReDraw()
+        self.doPaletteClick(event.X, event.Y)
+
+    def onPaletteUp(self, event):
+        """Stores state that the mouse is no longer depressed."""
+        self.mouse_down = False
+
+    def onPaletteMotion(self, event):
+        """Updates the colour values during mouse motion while the
+        mouse button is depressed."""
+        if self.mouse_down:
+            self.doPaletteClick(event.X, event.Y)
+
+    def doPaletteClick(self, m_x, m_y):
+        """Updates the colour values based on the mouse location
+        over the palette."""
+        # Highlight a fresh selected area
+        self.palette.ReDraw()
+        c = self.palette.highlight_xy(m_x, m_y)
         print "color", c
         self.colors[self.current_register] = c
         self.color_registers.Refresh()
@@ -187,10 +229,14 @@ if __name__ == "__main__":
     simple test for the dialog
     """
     a = wx.App(False)
+    import wx.lib.inspection
+    wx.lib.inspection.InspectionTool().Show()
     w = wx.Frame(None)
+    attr = w.GetDefaultAttributes()
     class Editor(object):
         playfield_colors = [40, 202, 148, 70, 0]
+        empty_color = attr.colBg.Get(False)
     e = Editor()
     d = AnticColorDialog(w, e)
-    d.ShowModal()
+    d.Show()
     a.MainLoop()
