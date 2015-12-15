5e2c9d9c9e0a8506e7ca2cf62de38b0e66508373
Rob McMullen <feedback@playermissile.com>
2015-12-14 16:01:54 -0800

    Updated version requirement for atrcopy to require segments with the label method

--- omnimon/__init__.py
+++ omnimon/__init__.py
@@ -11,7 +11,7 @@ __keywords__ = "text editor, python, plugins"
 __license__ = "GPL"
 __requires__ = [
     'numpy',
-    'atrcopy>=1.1.1',
+    'atrcopy>=1.2.0',
     'jsonpickle',
     'bson',
     'fs',
