20c5b35d9c843ecf2caff1abc082250658964fee
Rob McMullen <feedback@playermissile.com>
2016-02-10 13:58:47 -0800

    Added rectangular selection calculations

--- atrcopy.py
+++ atrcopy.py
@@ -288,6 +288,71 @@ class DefaultSegment(object):
                 start, end = end, start
             s[start:end] |= style_bits
     
+    def get_rect_indexes(self, anchor_start, anchor_end):
+        # determine row,col of upper left and lower right of selected
+        # rectangle.  The values are inclusive, so ul=(0,0) and lr=(1,2)
+        # is 2 rows and 3 columns.  Columns need to be adjusted slightly
+        # depending on quadrant of selection because anchor indexes are
+        # measured as cursor positions, that is: positions between the
+        # bytes where as rect select needs to think of the selections as
+        # on the byte positions themselves, not in between.
+        bpr = self.map_width
+        r1, c1 = divmod(anchor_start, bpr)
+        r2, c2 = divmod(anchor_end, bpr)
+        if c1 >= c2:
+            # start column is to the right of the end column so columns
+            # need to be swapped
+            if r1 >= r2:
+                # start row is below end row, so rows swapped as well
+                c1, c2 = c2, c1 + 1
+                r1, r2 = r2, r1
+            elif c2 == 0:
+                # When the cursor is at the end of a line, anchor_end points
+                # to the first character of the next line.  Handle this
+                # special case by pointing to end of the previous line.
+                c2 = bpr
+                r2 -= 1
+            else:
+                c1, c2 = c2 - 1, c1 + 1
+        else:
+            # start column is to the left of the end column, so don't need
+            # to swap columns
+            if r1 > r2:
+                # start row is below end row
+                r1, r2 = r2, r1
+                c2 += 1
+        anchor_start = r1 * bpr + c1
+        anchor_end = r2 * bpr + c2
+        r2 += 1
+        return anchor_start, anchor_end, (r1, c1), (r2, c2)
+    
+    def set_style_ranges_rect(self, ranges, **kwargs):
+        style_bits = self.get_style_bits(**kwargs)
+        s = self.style
+        for start, end in ranges:
+            start, end, (r1, c1), (r2, c2) = self.get_rect_indexes(start, end)
+            # Numpy tricks!
+            # >>> c1 = 15
+            # >>> r = 4 # r2 - r1
+            # >>> c = 10 # c2 - c1
+            # >>> width = 40
+            # >>> np.arange(c)
+            #array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+            # >>> np.arange(r) * width
+            #array([  0,  40,  80, 120])
+            # >>> np.tile(np.arange(c), r) + np.repeat(np.arange(r)*width, c)
+            #array([  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  40,  41,  42,
+            #        43,  44,  45,  46,  47,  48,  49,  80,  81,  82,  83,  84,  85,
+            #        86,  87,  88,  89, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129])
+            # >>> np.tile(np.arange(c), r) + np.repeat(np.arange(r)*width, c) + c1
+            #array([ 15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  55,  56,  57,
+            #        58,  59,  60,  61,  62,  63,  64,  95,  96,  97,  98,  99, 100,
+            #       101, 102, 103, 104, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144])
+            r = r2 - r1
+            c = c2 - c1
+            indexes = np.tile(np.arange(c), r) + np.repeat(np.arange(r) * self.map_width, c) + start
+            s[indexes] |= style_bits
+    
     def clear_style_bits(self, **kwargs):
         style_mask = self.get_style_mask(**kwargs)
         self.style &= style_mask
