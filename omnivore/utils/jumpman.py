fd5e9256ea2c08cb9c1849ab657a39be8ce0cd11
Rob McMullen <feedback@playermissile.com>
2016-06-07 23:33:02 -0700

    Added parsing of jumpman level drawing object codes * added unit test for jumpman stuff

--- omnivore/utils/jumpman.py
+++ omnivore/utils/jumpman.py
@@ -1,27 +1,25 @@
 import numpy as np
 
 class JumpmanLevelBuilder(object):
-    def __init__(self, screen, source):
-        self.screen = screen
-        self.clear_screen()
-        self.parse_commands(source)
+    def __init__(self, segments):
+        self.segments = segments
 
-    def clear_screen(self):
-        self.screen[:] = 0
+    def clear_screen(self, screen):
+        screen[:] = 0
 
-    def parse_commands(self, commands):
-        x = y = deltax = deltay = num = 0
+    def draw_commands(self, screen, commands):
+        self.clear_screen(screen)
+        x = y = dx = dy = num = 0
         addr = None
         index = 0
         last = len(commands)
         while index < last:
             c = commands[index]
+            print "index=%d, command=%x" % (index, c)
             index += 1
             if c < 0xfb:
-                if index < last:
-                    num = commands[index]
-                    self.draw(addr, x, y, deltax, deltay, num)
-                    index += 1
+                if addr is not None:
+                    self.draw(addr, x, y, dx, dy, c)
             elif index + 1 < last:
                 arg1 = commands[index]
                 arg2 = commands[index + 1]
@@ -32,11 +30,48 @@ class JumpmanLevelBuilder(object):
                     x = arg1
                     y = arg2
                 elif c == 0xfe:
-                    deltax = arg1
-                    deltay = arg2
+                    dx = arg1
+                    dy = arg2
                 elif c == 0xff:
                     return
 
-    def draw(self, addr, x, y, deltax, deltay, num):
-        pass
+    def draw(self, addr, x, y, dx, dy, repeat):
+        print "addr=%x x=%d y=%d dx=%d dy=%d, num=%d" % (addr, x, y, dx, dy, repeat)
+        codes = self.get_object_code(addr)
+        if codes is None:
+            return
+        print "  found codes:", codes
+        index = 0
+        last = len(codes)
+        while index < last:
+            prefix = list(codes[index:index + 3])
+            if len(prefix) < 3:
+                if prefix[0] != 0xff:
+                    print "  short prefix", prefix
+                return
+            n, xoffset, yoffset = prefix
+            index += 3
+            pixels = list(codes[index:index + n])
+            if len(pixels) < n:
+                print "  %d pixels expected, %d found" % (n, len(pixels))
+                return
+            print "pixels: n=%d x=%d y=%d pixels=%s" % (n, xoffset, yoffset, pixels)
+            index += n
+
+
+
+    def get_object_code(self, addr):
+        for s in self.segments:
+            index = addr - s.start_addr
+            if s.is_valid_index(index):
+                codes = s[index:]
+                end = np.nonzero(codes==255)[0]
+                print end
+                if len(end) > 0:
+                    print end
+                    end = end[0] + 1
+                else:
+                    continue
+                codes = s[index:index + end]
+                return codes
 
