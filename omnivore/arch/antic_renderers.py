997904f1e7289f046753a390ef49e15a0a259586
Rob McMullen <feedback@playermissile.com>
2016-11-15 10:53:15 -0800

    Fixed #91: added byte mapping routines to font_mapping classes in antic_renderers.py

--- omnivore/arch/antic_renderers.py
+++ omnivore/arch/antic_renderers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import wx
+
 from atrcopy import match_bit_mask, comment_bit_mask, data_bit_mask, selected_bit_mask, diff_bit_mask, not_user_bit_mask
 
 from omnivore.utils.permute import bit_reverse_table
@@ -824,10 +826,75 @@ class ATASCIIFontMapping(object):
     name = "ATASCII Characters"
     font_mapping = atascii_to_internal
 
-class AnticFontMapping(object):
+    def wx_char_to_byte(self, char, mods, control):
+        byte = None
+
+        if mods == wx.MOD_RAW_CONTROL:
+            if char == 44:  # Ctrl-, prints ATASCII 0 (heart)
+                byte = 0 + control.inverse
+            elif char >= 65 and char <= 90:  # Ctrl-[A-Z] prints ATASCII chars 1-26
+                byte = char - 64 + control.inverse
+            elif char == 46:  # Ctrl-. prints ATASCII 96 (diamond)
+                byte = 96 + control.inverse
+            elif char == 59:  # Ctrl-; prints ATASCII 123 (spade)
+                byte = 123 + control.inverse
+            elif char == wx.WXK_TAB:
+                byte = 158
+            elif char == 50:  # Ctrl-2 prints ATASCII 253 (buzzer)
+                byte = 253
+            elif char == wx.WXK_INSERT:
+                byte = 255
+        elif mods == wx.MOD_SHIFT:
+            if char == wx.WXK_BACK:
+                byte = 156
+            elif char == wx.WXK_INSERT:
+                byte = 157
+            elif char == wx.WXK_TAB:
+                byte = 159
+        elif char == wx.WXK_HOME:
+            byte = 125
+        elif char == wx.WXK_BACK:
+            byte = 126
+        elif char == wx.WXK_TAB:
+            byte = 127
+        elif char == wx.WXK_RETURN:
+            byte = 155
+        elif char == wx.WXK_DELETE:
+            byte = 254
+        elif char == wx.WXK_INSERT:
+            byte = 255
+        
+        elif control.pending_esc:
+            if char == wx.WXK_ESCAPE:
+                byte = 27
+            elif char == wx.WXK_UP:
+                byte = 28
+            elif char == wx.WXK_DOWN:
+                byte = 29
+            elif char == wx.WXK_LEFT:
+                byte = 30
+            elif char == wx.WXK_RIGHT:
+                byte = 31
+        
+        elif char == wx.WXK_ESCAPE:
+            control.pending_esc = True
+
+        return byte
+
+    def convert_byte_mapping(self, char):
+        return char
+
+
+class AnticFontMapping(ATASCIIFontMapping):
     name = "Antic Map"
     font_mapping = np.arange(256, dtype=np.uint8)
 
+    def convert_byte_mapping(self, char):
+        try:
+            char = atascii_to_internal[char]
+        except IndexError:
+            pass
+        return char
 
 
 class BytePerPixelMemoryMap(BaseRenderer):
--- omnivore/utils/wx/bitviewscroller.py
+++ omnivore/utils/wx/bitviewscroller.py
@@ -827,6 +827,7 @@ class FontMapScroller(BitviewScroller):
             return
         char = evt.GetUniChar()
         if char > 0:
+            char = self.editor.machine.font_mapping.convert_byte_mapping(char)
             self.change_byte(char | self.inverse)
     
     def change_byte(self, value):
@@ -858,67 +859,20 @@ class FontMapScroller(BitviewScroller):
             else:
                 delta_index = self.process_movement_keys(char)
         else:
-            if mods == wx.MOD_RAW_CONTROL:
-                if char == 44:  # Ctrl-, prints ATASCII 0 (heart)
-                    byte = 0 + self.inverse
-                elif char >= 65 and char <= 90:  # Ctrl-[A-Z] prints ATASCII chars 1-26
-                    byte = char - 64 + self.inverse
-                elif char == 46:  # Ctrl-. prints ATASCII 96 (diamond)
-                    byte = 96 + self.inverse
-                elif char == 59:  # Ctrl-; prints ATASCII 123 (spade)
-                    byte = 123 + self.inverse
-                elif char == wx.WXK_TAB:
-                    byte = 158
-                elif char == 50:  # Ctrl-2 prints ATASCII 253 (buzzer)
-                    byte = 253
-                elif char == wx.WXK_INSERT:
-                    byte = 255
-            elif mods == wx.MOD_SHIFT:
-                if char == wx.WXK_BACK:
-                    byte = 156
-                elif char == wx.WXK_INSERT:
-                    byte = 157
-                elif char == wx.WXK_TAB:
-                    byte = 159
-            elif char == wx.WXK_HOME:
-                byte = 125
-            elif char == wx.WXK_BACK:
-                byte = 126
-            elif char == wx.WXK_TAB:
-                byte = 127
-            elif char == wx.WXK_RETURN:
-                byte = 155
-            elif char == wx.WXK_DELETE:
-                byte = 254
-            elif char == wx.WXK_INSERT:
-                byte = 255
-            
-            elif char == wx.WXK_F1:
-                self.inverse = (self.inverse + 0x80) & 0x80
-                self.set_status_message()
-            elif char == wx.WXK_F2:
-                self.editing = False
-                self.set_status_message()
-            
-            elif self.pending_esc:
-                if char == wx.WXK_ESCAPE:
-                    byte = 27
-                elif char == wx.WXK_UP:
-                    byte = 28
-                elif char == wx.WXK_DOWN:
-                    byte = 29
-                elif char == wx.WXK_LEFT:
-                    byte = 30
-                elif char == wx.WXK_RIGHT:
-                    byte = 31
-            
-            elif char == wx.WXK_ESCAPE:
-                self.pending_esc = True
-        
-            else:
-                delta_index = self.process_movement_keys(char)
+            byte = self.editor.machine.font_mapping.wx_char_to_byte(char, mods, self)
+            if byte is None:
+                if char == wx.WXK_F1:
+                    self.inverse = (self.inverse + 0x80) & 0x80
+                    self.set_status_message()
+                elif char == wx.WXK_F2:
+                    self.editing = False
+                    self.set_status_message()
+                
+                else:
+                    delta_index = self.process_movement_keys(char)
         
         if byte is not None:
+            byte = self.editor.machine.font_mapping.convert_byte_mapping(byte)
             self.change_byte(byte)
             self.pending_esc = False
         elif delta_index is not None:
