b8033d54df246e92a0cd22b375fa6e144f547407
Rob McMullen <feedback@playermissile.com>
2016-12-13 11:27:51 -0800

    Added file utilities: get latest file from a path specifier with wildcards

---
+++ omnivore/utils/fileutil.py
@@ -0,0 +1,11 @@
+import os
+import glob
+
+def get_latest_file(pathspec):
+    files = glob.glob(pathspec)
+    if files:
+        # for f in files:
+        #     print os.path.getctime(f), os.path.getmtime(f), f
+        newest = max(files, key=os.path.getmtime)
+        return newest
+    return pathspec
