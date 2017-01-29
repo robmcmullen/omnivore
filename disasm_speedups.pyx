710aa0ae417ac0377fa7ccaac627da239e0d2e17
Rob McMullen <feedback@playermissile.com>
2017-01-28 22:11:59 -0800

    Fixed most output problems; somehow puts a zero byte at the end of the string so the diffs to the slow version don't compare exactly

--- disasm_speedups.pyx
+++ disasm_speedups.pyx
@@ -2,8 +2,6 @@ from __future__ import division
 import cython
 import numpy as np
 cimport numpy as np
-from libc.stdio cimport printf
-from libc.string cimport strncpy
 
 cdef extern:
     int parse_instruction_c(char *wrap, int pc, char *src, int last_pc)
@@ -17,30 +15,26 @@ def get_disassembled_chunk_fast(storage_wrapper, np.ndarray[char, ndim=1, mode="
     cdef char *storage = storage_wrapper_array.data
     cdef int c_index = index_of_pc
     cdef char *binary = binary_array.data + c_index
-    cdef int c_pc, c_last, count, line, max_lines
-    cdef char out[80]
+    cdef int c_pc, c_last, count, row, max_rows
 
     c_pc = pc
     c_last = last
-    line = storage_wrapper.row
-    max_lines = storage_wrapper.num_rows
+    row = storage_wrapper.row
+    max_rows = storage_wrapper.num_rows
 
     # fast loop in C
-    while c_pc < c_last and line < max_lines:
+    while c_pc < c_last and row < max_rows:
         count = parse_instruction_c(storage, c_pc, binary, c_last)
-        strncpy(out, storage, 48)
-        out[48] = '\n'
-        out[49] = '\0'
-        printf("%s\n", out)
         if count == 0:
             break
         c_pc += count
         c_index += count
-        storage += count
+        storage += 48
         binary += count
-        line += 1
+        row += 1
 
     # get data back out in python vars
     pc = c_pc
     index_of_pc = c_index
+    storage_wrapper.row = row
     return pc, index_of_pc
