a1cf88eeb635cca2c2edc2ad308dae4602c4ac13
Rob McMullen <feedback@playermissile.com>
2017-02-18 11:59:22 -0800

    Changed API call to disassembler chunk assigmnent

--- omnivore/tasks/hex_edit/disassembly.py
+++ omnivore/tasks/hex_edit/disassembly.py
@@ -49,10 +49,10 @@ class DisassemblyTable(ByteGridTable):
         self._rows = 0
         self.disassembler = editor.machine.get_disassembler(editor.task.hex_grid_lower_case, editor.task.assembly_lower_case)
         disasm = self.disassembler.fast
-        disasm.add_data_processor(1)
-        disasm.add_antic_dl_processor(2)
-        disasm.add_jumpman_level_processor(3)
-        disasm.add_jumpman_harvest_processor(4)
+        disasm.add_chunk_processor("data", 1)
+        disasm.add_chunk_processor("antic_dl", 2)
+        disasm.add_chunk_processor("jumpman_level", 3)
+        disasm.add_chunk_processor("jumpman_harvest", 4)
         self.hex_lower = editor.task.hex_grid_lower_case
         if self.hex_lower:
             self.fmt_hex2 = "%02x"
