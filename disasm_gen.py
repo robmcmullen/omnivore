aca48908ea1220cfcfeb9c5dba4f81d4381e9de4
Rob McMullen <feedback@playermissile.com>
2017-02-28 17:49:20 -0800

    Fixed argument list of generated parse functions

--- disasm_gen.py
+++ disasm_gen.py
@@ -257,7 +257,7 @@ class PyxGenerator(object):
 
     def gen_pyx(self):
         filename = "udis_fast/disasm_speedups_monolithic.pyx"
-        prototype_arglist = "(char *wrap, char *src, int pc, int last_pc, np.uint16_t *labels, char *instructions, int strpos)"
+        prototype_arglist = "(char *wrap, char *src, int pc, int last_pc, np.uint16_t *labels, char *instructions, int strpos, int mnemonic_lower, char *hexdigits)"
         externlist = []
         for n in self.function_name_list:
             externlist.append("    int %s%s" % (n, prototype_arglist))
@@ -273,7 +273,10 @@ import cython
 import numpy as np
 cimport numpy as np
 
-ctypedef int (*parse_func_t)(char *, char *, int, int, np.uint16_t *, char *, int)
+ctypedef int (*parse_func_t)(char *, char *, int, int, np.uint16_t *, char *, int, int, char *)
+
+cdef char *hexdigits_lower = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
+cdef char *hexdigits_upper = "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7C8C9CACBCCCDCECFD0D1D2D3D4D5D6D7D8D9DADBDCDDDEDFE0E1E2E3E4E5E6E7E8E9EAEBECEDEEEFF0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF"
 
 cdef extern:
 $EXTERNLIST
@@ -311,10 +314,14 @@ $DEFLIST
     c_pc = pc
     c_last = last
     max_rows = storage_wrapper.num_rows
+    if hex_lower:
+        hexdigits_for_case = hexdigits_lower
+    else:
+        hexdigits_for_case = hexdigits_upper
 
     # fast loop in C
     while c_pc < c_last and row < max_rows and strpos < max_strpos:
-        count = parse_func(metadata, binary, c_pc, c_last, labels, instructions, strpos)
+        count = parse_func(metadata, binary, c_pc, c_last, labels, instructions, strpos, mnemonic_lower, hexdigits_for_case)
         if count == 0:
             break
         elif count == 1:
