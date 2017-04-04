cd7e6f5481eb8e5ab977ef8e9838122b241bd3f9
Rob McMullen <feedback@playermissile.com>
2017-04-04 12:13:34 -0700

    Refs #151: removed segment list from menu, changed Segments to Segment

--- omnivore8bit/hex_edit/task.py
+++ omnivore8bit/hex_edit/task.py
@@ -72,7 +72,7 @@ class HexEditTask(FrameworkTask):
 
     def _extra_actions_default(self):
         data_menu = self.create_menu("Menu", "Disk Image", "ParserGroup", "EmulatorGroup", "ActionGroup")
-        segment_menu = self.create_menu("Menu", "Segments", "ListGroup", "ActionGroup")
+        segment_menu = self.create_menu("Menu", "Segment", "ActionGroup")
         bytes_menu = self.create_menu("Menu", "Bytes", "HexModifyGroup")
         actions = [
             # Menubar additions
@@ -357,7 +357,7 @@ class HexEditTask(FrameworkTask):
             SegmentGotoAction(),
             ]
 
-    def get_actions_Menu_Segments_ActionGroup(self):
+    def get_actions_Menu_Segment_ActionGroup(self):
         return [
             GetSegmentFromSelectionAction(),
             MultipleSegmentsFromSelectionAction(),
@@ -382,13 +382,6 @@ class HexEditTask(FrameworkTask):
             AddTraceStartPointAction(),
             ApplyTraceSegmentAction(),
             ClearTraceAction(),
-            ]
-
-    def get_actions_Menu_Segments_ListGroup(self):
-        return [
-            SMenu(
-                SegmentChoiceGroup(id="a2", separator=True),
-                id='segmentlist1', separator=False, name="View Segment"),
             ]
 
     def get_actions_Menu_Bytes_HexModifyGroup(self):
