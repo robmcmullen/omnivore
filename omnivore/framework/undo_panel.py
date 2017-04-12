e93a4774ff3b7c6bb61c182eb68a4306b01e4fef
Rob McMullen <feedback@playermissile.com>
2017-04-11 19:20:18 -0700

    Fixed #182: pops down when it's a control; doesn't work when it's a panel

--- omnivore/framework/undo_panel.py
+++ omnivore/framework/undo_panel.py
@@ -5,24 +5,10 @@ import traceback
 import wx
 
 
-class UndoHistoryPanel(wx.Panel):
+class UndoHistoryPanel(wx.ListBox):
     def __init__(self, parent, task, **kwargs):
         self.task = task
-        wx.Panel.__init__(self, parent, wx.ID_ANY, **kwargs)
-
-        # Mac/Win needs this, otherwise background color is black
-        attr = self.GetDefaultAttributes()
-        self.SetBackgroundColour(attr.colBg)
-
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-
-        self.history = wx.ListBox(self, size=(100, -1))
-
-        self.sizer.Add(self.history, 1, wx.EXPAND)
-
-        self.SetSizer(self.sizer)
-        self.sizer.Layout()
-        self.Fit()
+        wx.ListBox.__init__(self, parent, wx.ID_ANY, **kwargs)
 
         self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
 
@@ -59,10 +45,10 @@ class UndoHistoryPanel(wx.Panel):
     def update_history(self):
         project = self.task.active_editor
         summary = project.document.undo_stack.history_list()
-        self.history.Set(summary)
+        self.Set(summary)
         index = project.document.undo_stack.insert_index
         if index > 0:
-            self.history.SetSelection(index - 1)
+            self.SetSelection(index - 1)
 
     def activate_spring_tab(self):
         self.recalc_view()
