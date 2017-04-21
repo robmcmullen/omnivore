c01d4f51286e045ae2e3f7658aeb06d02499e03a
Rob McMullen <feedback@playermissile.com>
2017-04-21 13:51:09 -0700

    Pulled out help directory list so it can be overridden by the application

--- omnivore/help/__init__.py
+++ omnivore/help/__init__.py
@@ -10,18 +10,27 @@ class MissingDocumentationError(RuntimeError):
     pass
 
 
+root_resource_path = None
+
+help_dirs = ["omnivore/help", "omnivore8bit/help"]
+
+
 def get_htmlhelp(name):
+    global root_resource_path
+
     # resource path will point to the omnivore/templates directory
-    path = get_resource_path(1)
-    log.debug("resource path: %s" % path)
+    if root_resource_path is None:
+        path = get_resource_path(1)
+        root_resource_path = os.path.normpath(os.path.join(path, "../.."))
+        log.debug("resource path: %s" % root_resource_path)
     attempts = []
-    for toplevel in ["", "../../omnivore8bit/help"]:
-        pathname = os.path.normpath(os.path.join(path, toplevel, name))
+    for subdir in help_dirs:
+        pathname = os.path.normpath(os.path.join(root_resource_path, subdir, name))
         log.debug("Checking for htmlhelp at %s" % pathname)
         if os.path.exists(pathname):
             log.debug("Found htmlhelp for %s: %s" % (name, pathname))
             return pathname
         attempts.append(pathname)
     else:
-        log.debug("No htmlhelp found for %s in %s" % (name, path))
+        log.debug("No htmlhelp found for %s in %s" % (name, attempts))
         raise MissingDocumentationError("Unable to locate %s help files; installation error?\nThe files should be in one of the following:\n%s\n\nbut were not found." % (name, "\n  * " + "\n  * ".join([os.path.dirname(d) for d in attempts])))
