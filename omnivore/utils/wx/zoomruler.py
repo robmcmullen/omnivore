5c6a8e94dc9e033199a5ba67187f9910ec9fad74
Rob McMullen <feedback@playermissile.com>
2017-06-02 13:52:49 -0700

    Added labeling to ZoomRuler

--- omnivore/utils/wx/zoomruler.py
+++ omnivore/utils/wx/zoomruler.py
@@ -1,26 +1,102 @@
+import random
+
 import wx
 from wx.lib.agw.rulerctrl import RulerCtrl
 import wx.lib.scrolledpanel as scrolled
 
 
-class ZoomRuler(scrolled.ScrolledPanel):
+class LabeledRuler(RulerCtrl):
+    def __init__(self, *args, **kwargs):
+        RulerCtrl.__init__(self, *args, **kwargs)
+        self._marks = {}
+        self._mark_pen = wx.Pen(wx.RED)
+
+    def position_to_value(self, pos):
+        pos = min(pos, self._right) * 1.0  # make it float
+        value = (self._max - self._min)*(pos/self._right)  # will be float
+        return value
+
+    def value_to_position(self, value):
+        perc = (value - self._min)/(self._max - self._min)
+        if perc >= 0.0 and perc <= 1.0:
+            pos = perc * self._right
+        else:
+            pos = None
+        print "value_to_position", value, perc, pos
+        return pos
+
+    def set_mark(self, value, text):
+        self._marks[value] = text
+
+    def draw_mark(self, dc, pos):
+        length = 10
+        if self._orientation == wx.HORIZONTAL:
+            if self._flip:
+                dc.DrawLine(self._left + pos, self._top,
+                            self._left + pos, self._top + length)
+            else:
+                dc.DrawLine(self._left + pos, self._bottom - length,
+                            self._left + pos, self._bottom)
+        
+        else:
+            if self._flip:
+                dc.DrawLine(self._left, self._top + pos,
+                            self._left + length, self._top + pos)
+            else:
+                dc.DrawLine(self._right - length, self._top + pos,
+                            self._right, self._top + pos)
+
+    def Draw(self, dc):
+        RulerCtrl.Draw(self, dc)
+
+        dc.SetBrush(wx.Brush(self._background))
+        dc.SetPen(self._mark_pen)
+
+        for mark, label in self._marks.iteritems():
+            pos = self.value_to_position(mark)
+            if pos is None:
+                # skip offscreen marks
+                continue
+            self.draw_mark(dc, pos)
+
+
+
+
+class ZoomRuler(wx.Panel):
     """Zoomable ruler that uses a scrollbar and resize to implement the zoom.
     """
     def __init__(self, parent, **kwargs):
-        scrolled.ScrolledPanel.__init__(self, parent, -1, size=(-1, 50), style=wx.HSCROLL)
-        self.ruler = RulerCtrl(self, -1)
+        wx.Panel.__init__(self, parent, -1, **kwargs)
+        self.panel = scrolled.ScrolledPanel(self, -1, size=(-1, 50), style=wx.HSCROLL)
+        self.ruler = LabeledRuler(self.panel, -1)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.ruler, 1, wx.EXPAND, 0)
-        self.SetSizer(sizer)
-        self.ShowScrollbars(wx.SHOW_SB_ALWAYS, wx.SHOW_SB_NEVER)
-        self.SetupScrolling(scroll_y=False)
+        self.panel.SetSizer(sizer)
+        self.panel.ShowScrollbars(wx.SHOW_SB_ALWAYS, wx.SHOW_SB_NEVER)
+        self.panel.SetupScrolling(scroll_y=False)
         sizer.Layout()
+        self.panel.Fit()
+
+        self.label_min = wx.StaticText(self, -1, "Min")
+        self.label_max = wx.StaticText(self, -1, "Max", style=wx.ALIGN_RIGHT)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox.Add(self.label_min, 0, wx.EXPAND, 0)
+        hbox.Add((0, 0), 1)
+        hbox.Add(self.label_max, 0, wx.EXPAND, 0)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.panel, 1, wx.EXPAND, 0)
+        sizer.Add(hbox, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer)
         self.Fit()
 
         size = (1000,40)
         self.zoom_parent(size)
+        for i in range(20):
+            self.ruler.set_mark(random.uniform(1.0, 10.0), "Whatever!")
 
-        #self.panel.Bind(wx.EVT_SIZE, self.panel_size)
+        self.panel.Bind(wx.EVT_SCROLLWIN, self.on_scroll)
         self.ruler.Bind(wx.EVT_MOUSE_EVENTS, self.on_mouse_events)
 
     def on_mouse_events(self, event):
@@ -49,7 +125,23 @@ class ZoomRuler(scrolled.ScrolledPanel):
 
     def zoom_parent(self, size):
         self.ruler.SetSize(size)
-        self.SetVirtualSize(size)
+        self.panel.SetVirtualSize(size)
+        self.update_limits()
+
+    def update_limits(self):
+        x, y = self.panel.GetViewStart()
+        x1, y1 = self.panel.CalcUnscrolledPosition(x, y)
+        print "view", x, y, "unscrolled", x1, y1
+        print "page", self.panel.GetScrollPageSize(wx.HSCROLL)
+        print "parent", self.GetSize()
+        left = self.ruler.position_to_value(x1)
+        right = self.ruler.position_to_value(x1 + self.GetSize()[0] - 1)
+        print left, right
+        self.label_min.SetLabel("%.3f" % left)
+        self.label_max.SetLabel("%.3f" % right)
+
+    def on_scroll(self, evt):
+        self.update_limits()
 
 
 if __name__ == "__main__":
