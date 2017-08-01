b91112778081919c75da478e28535d9829f471b8
Rob McMullen <feedback@playermissile.com>
2017-08-01 15:09:04 -0700

    Removed funky numpy build_ext hack from omnivore-framework

--- omnivore-framework/setup.py
+++ omnivore-framework/setup.py
@@ -36,19 +36,6 @@ install_requires = [
 
 cmdclass = dict()
 
-from setuptools.command.build_ext import build_ext as _build_ext
-
-# Bootstrap numpy so that numpy can be installed as a dependency, from:
-# http://stackoverflow.com/questions/19919905
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-cmdclass["build_ext"] = build_ext
-
 import omnivore
 full_version = omnivore.__version__
 spaceless_version = full_version.replace(" ", "_")
