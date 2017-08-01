e71bc2c73dfcd980e07cfe80427fd3be28de0be8
Rob McMullen <feedback@playermissile.com>
2017-07-31 22:52:30 -0700

    Added code to create "omnivore-framework" project, a subset for PyPI that MapRoom can install directly

---
+++ Makefile
@@ -0,0 +1,40 @@
+VERSION := $(shell python -c "import omnivore; print(omnivore.__version__)")
+SDIST := omnivore-framework/dist/omnivore-framework-${VERSION}.tar.gz
+
+all: framework
+
+omnivore-framework/omnivore:
+	ln -s ../omnivore omnivore-framework
+
+omnivore-framework/traits:
+	ln -s ../deps/traits/traits omnivore-framework
+
+omnivore-framework/traitsui:
+	ln -s ../deps/traitsui/traitsui omnivore-framework
+
+omnivore-framework/apptools:
+	ln -s ../deps/apptools/apptools omnivore-framework
+
+omnivore-framework/envisage:
+	ln -s ../deps/envisage/envisage omnivore-framework
+
+omnivore-framework/pyface:
+	ln -s ../deps/pyface/pyface omnivore-framework
+
+omnivore-framework/fs:
+	ln -s ../deps/pyfilesystem/fs omnivore-framework
+
+omnivore-framework/LICENSE:
+	ln -s ../deps/pyfilesystem/LICENSE omnivore-framework
+
+omnivore-framework/LICENSE.Enthought:
+	ln -s ../deps/pyfilesystem/LICENSE.Enthought omnivore-framework
+
+${SDIST}:
+	(cd omnivore-framework; python setup.py sdist)
+
+framework: omnivore-framework/omnivore omnivore-framework/traits omnivore-framework/traitsui omnivore-framework/apptools omnivore-framework/envisage omnivore-framework/pyface omnivore-framework/fs omnivore-framework/LICENSE omnivore-framework/LICENSE.Enthought ${SDIST}
+
+clean:
+	rm -rf omnivore-framework/dist
+	rm -f omnivore-framework/LICENSE*
---
+++ omnivore-framework/setup.py
@@ -0,0 +1,204 @@
+# setup file for only the framework part of Omnivore. Includes:
+#
+# Enthought (traits, traitsui, pyface, apptools, envisage)
+# pyfilesystem
+# 
+# Doesn't include:
+#
+# omnivore8bit
+# omnivore_extra
+# udis
+#
+import os
+import sys
+import shutil
+import glob
+import subprocess
+from setuptools import find_packages
+from setuptools import setup
+from distutils.extension import Extension
+
+ext_modules = [
+    Extension("traits.ctraits",
+              sources = ["traits/ctraits.c"],
+              extra_compile_args = ["-DNDEBUG=1", "-O3" ]#, '-DPy_LIMITED_API'],
+              ),
+    ]
+
+install_requires = [
+    'numpy',
+    'jsonpickle>=0.9.4',
+    'bson<1.0.0',
+    'configobj',
+    'pyparsing',
+    ]
+
+
+cmdclass = dict()
+
+from setuptools.command.build_ext import build_ext as _build_ext
+
+# Bootstrap numpy so that numpy can be installed as a dependency, from:
+# http://stackoverflow.com/questions/19919905
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+cmdclass["build_ext"] = build_ext
+
+import omnivore
+full_version = omnivore.__version__
+spaceless_version = full_version.replace(" ", "_")
+
+common_includes = [
+    "ctypes",
+    "ctypes.util",
+    "wx.lib.pubsub.*",
+    "wx.lib.pubsub.core.*",
+    "wx.lib.pubsub.core.kwargs.*",
+    "multiprocessing",
+    "pkg_resources",
+    "configobj",
+    
+    "traits",
+    
+    "traitsui",
+    "traitsui.editors",
+    "traitsui.editors.*",
+    "traitsui.extras",
+    "traitsui.extras.*",
+    "traitsui.wx",
+    "traitsui.wx.*",
+ 
+    "pyface",
+    "pyface.*",
+    "pyface.wx",
+ 
+    "pyface.ui.wx",
+    "pyface.ui.wx.init",
+    "pyface.ui.wx.*",
+    "pyface.ui.wx.grid.*",
+    "pyface.ui.wx.action.*",
+    "pyface.ui.wx.timer.*",
+    "pyface.ui.wx.tasks.*",
+    "pyface.ui.wx.workbench.*",
+]
+common_includes.extend(omnivore.get_py2exe_toolkit_includes())
+
+py2app_includes = [
+]
+
+common_excludes = [
+    "test",
+#    "unittest", # needed for numpy
+    "pydoc_data",
+    "pyface.ui.qt4",
+    "traitsui.qt4",
+     "Tkconstants",
+    "Tkinter", 
+    "tcl", 
+    "_imagingtk",
+    "PIL._imagingtk",
+    "ImageTk",
+    "PIL.ImageTk",
+    "FixTk",
+    ]
+
+py2exe_excludes = [
+    ]
+
+# package_data is for pip and python installs, not for app bundles. Need
+# to use data_files for that
+package_data = {
+    '': ['images/*',
+         '*.ini',
+         ],
+    'apptools': ['help/help_plugin/*.ini',
+                 'help/help_plugin/action/images/*.png',
+                 'logger/plugin/*.ini',
+                 'logger/plugin/view/images/*.png',
+                 'naming/ui/images/*.png',
+                 ],
+    'traitsui': ['image/library/*.zip',
+                 'wx/images/*',
+                 'qt4/images/*',
+                 ],
+    'pyface': ['images/*',
+               'dock/images/*',
+               'tree/images/*',
+               'tests/images/*',
+               'action/images/*',
+               'ui/wx/images/*',
+               'ui/wx/grid/images/*',
+               ],
+    'omnivore': ['icons/*.png',
+                 'icons/*.ico',
+                 'utils/*.png',
+                 'templates/*',
+                 ],
+    }
+
+all_packages = find_packages()
+
+# cull packages
+packages = []
+for p in all_packages:
+    if p.startswith("omnivore8bit") or p.startswith("omnivore_extra") or p.startswith("udis"):
+        continue
+    packages.append(p)
+
+base_dist_dir = "dist-%s" % spaceless_version
+win_dist_dir = os.path.join(base_dist_dir, "win")
+mac_dist_dir = os.path.join(base_dist_dir, "mac")
+
+is_64bit = sys.maxsize > 2**32
+
+options = {}
+
+# data files are only needed when building an app bundle
+data_files = []
+
+setup(
+    name = 'omnivore-framework',
+    version = full_version,
+    author = omnivore.__author__,
+    author_email = omnivore.__author_email__,
+    url = omnivore.__url__,
+    download_url = ('%s/%s.tar.gz' % (omnivore.__download_url__, full_version)),
+    classifiers = [c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        License :: OSI Approved :: GNU General Public License (GPL)
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: Python
+        Topic :: Utilities
+        Topic :: Software Development :: Assemblers
+        Topic :: Software Development :: Disassemblers
+        """.splitlines() if len(c.strip()) > 0],
+    description = "The Atari 8-bit binary editor sponsored by the Player/Missile Podcast.",
+    long_description = open('README.rst').read(),
+    cmdclass = cmdclass,
+    ext_modules = ext_modules,
+    install_requires = install_requires,
+    setup_requires = ["numpy"],
+    license = "GPL",
+    packages = packages,
+    package_data = package_data,
+    data_files=data_files,
+    entry_points = {
+          'pyface.toolkits': [
+              'wx = pyface.ui.wx.init:toolkit_object',
+              'null = pyface.ui.null.init:toolkit_object',
+          ],
+    },
+    options=options,
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix"],
+    zip_safe = False,
+    )
