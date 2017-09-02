38c567369a400bbb54b9c905ba519a0f9ede3deb
Rob McMullen <feedback@playermissile.com>
2017-09-01 21:52:59 -0700

    pyinstaller: added hook script for the future module * added debug option for windows builds that prints to console

---
+++ pyinstaller/hook-future.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+    Future has some dynamic imports that are not picked up
+
+"""
+
+hiddenimports = ['UserList', 'UserDict', 'UserString', 'itertools', 'stat', 'base64', 'commands']
