671bc38018daff9901ca93b002c7c7a8ca2490b6
Rob McMullen <feedback@playermissile.com>
2017-11-17 13:52:35 -0800

    Updated framework release number to rc11

--- omnivore-framework/setup.py
+++ omnivore-framework/setup.py
@@ -37,7 +37,7 @@ install_requires = [
 cmdclass = dict()
 
 import omnivore
-full_version = "1.0rc10"
+full_version = "1.0rc11"
 spaceless_version = full_version.replace(" ", "_")
 
 common_includes = [
