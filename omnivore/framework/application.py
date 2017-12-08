45db9f9a37f4ade0ce6101792d640efcab66bc3a
Rob McMullen <feedback@playermissile.com>
2017-12-08 11:13:07 -0800

    Separated out config file load/save routines into persistence.py

--- omnivore/framework/application.py
+++ omnivore/framework/application.py
@@ -2,9 +2,7 @@ import os
 import sys
 import time
 import argparse
-import json
 
-import jsonpickle
 from datetime import datetime
 import fs
 import wx
@@ -20,6 +18,7 @@ from apptools.preferences.api import Preferences
 
 # Local imports.
 from .enthought_api import FrameworkTaskWindow
+from .persistence import FilePersistenceMixin
 from filesystem import init_filesystems
 from document import BaseDocument
 import documentation
@@ -39,7 +38,7 @@ def _task_window_wx_on_mousewheel(self, event):
         self.active_task._wx_on_mousewheel_from_window(event)
 
 
-class FrameworkApplication(TasksApplication):
+class FrameworkApplication(TasksApplication, FilePersistenceMixin):
     """ The sample framework Tasks application.
     """
 
@@ -554,57 +553,8 @@ class FrameworkApplication(TasksApplication):
         """Override the envisage.application method to force the use of standard
         config directory location instead of ~/.enthought 
         """
-
-        from omnivore.third_party.appdirs import user_config_dir, user_log_dir, user_cache_dir, user_data_dir
-        dirname = user_config_dir(self.name)
-        ETSConfig.application_home = dirname
-
-        # Make sure it exists!
-        if not os.path.exists(ETSConfig.application_home):
-            os.makedirs(ETSConfig.application_home)
-
-        dirname = user_log_dir(self.name)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        self.log_dir = dirname
-
-        self.log_file_ext = "-%s" % datetime.now().strftime("%Y%m%d-%H%M%S")
-
-        dirname = user_cache_dir(self.name)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        self.cache_dir = dirname
-
-        dirname = user_data_dir(self.name)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        self.user_data_dir = dirname
-
-        # Prevent py2exe from creating a dialog box on exit saying that there
-        # are error messages.  It thinks that anything written to stderr is an
-        # error, and the python logging module redirects everything to stderr.
-        # Instead, redirect stderr to a log file in the user log directory
-        frozen = getattr(sys, 'frozen', False)
-        if frozen in ('dll', 'windows_exe', 'console_exe'):
-            # redirect py2exe stderr/stdout to log file
-            log = self.get_log_file_name("py2exe")
-            oldlog = sys.stdout
-            sys.stdout = open(log, 'w')
-            if hasattr(oldlog, "saved_text"):
-                sys.stdout.write("".join(oldlog.saved_text))
-            sys.stderr = sys.stdout
-
-            # The logging module won't redirect to the new stderr without help
-            handler = logging.StreamHandler(sys.stderr)
-            logger = logging.getLogger('')
-            logger.addHandler(handler)
-        else:
-            log = self.get_log_file_name("log")
-            handler = logging.FileHandler(log)
-            formatter = logging.Formatter("%(levelname)s:%(name)s:%(msg)s")
-            handler.setFormatter(formatter)
-            logger = logging.getLogger('')
-            logger.addHandler(handler)
+        self.setup_file_persistence(self.name)
+        ETSConfig.application_home = self.app_home_dir
 
     #### Convenience methods
 
@@ -673,87 +623,6 @@ class FrameworkApplication(TasksApplication):
                 self.preferences.dump()
             helper = helper_object(preferences=self.preferences)
         return helper
-
-    def get_log_file_name(self, log_file_name_base, ext=""):
-        filename = log_file_name_base + self.log_file_ext
-        if ext:
-            if not ext.startswith("."):
-                filename += "."
-            filename += ext
-        else:
-            filename += ".log"
-        filename = os.path.join(self.log_dir, filename)
-        return filename
-
-    def save_log(self, text, log_file_name_base, ext=""):
-        filename = self.get_log_file_name(log_file_name_base, ext)
-
-        try:
-            with open(filename, "wb") as fh:
-                fh.write(text)
-        except IOError:
-            log.error("Failed writing %s to %s" % (log_file_name_base, filename))
-
-    def get_config_dir_filename(self, subdir, json_name):
-        config_dir = ETSConfig.application_home
-        dirname = os.path.join(config_dir, subdir)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        return os.path.join(dirname, json_name)
-
-    def get_json_data(self, json_name, default_on_error=None):
-        try:
-            file_path = self.get_config_dir_filename("json", json_name)
-            with open(file_path, "r") as fh:
-                raw = fh.read()
-            json_data = jsonpickle.decode(raw)
-            try:
-                # new format is a list with a format identifier as the first entry
-                if json_data[0] == "format=v2":
-                    decoded = json_data[1]
-            except KeyError:
-                # deprecated format was a dictionary, and was creating an extra
-                # layer of indirection by encoding the jsonpickle as another string
-                json_data = json.loads(raw)
-                encoded = json_data[json_name]
-                decoded = jsonpickle.decode(encoded)
-            return decoded
-        except IOError:
-            # file not found
-            return default_on_error
-        except ValueError:
-            # bad JSON format
-            log.error("Bad JSON format in preferences file: %s" % json_name)
-            return default_on_error
-
-    def save_json_data(self, json_name, data):
-        file_path = self.get_config_dir_filename("json", json_name)
-        json_data = ["format=v2", data]
-        encoded = jsonpickle.encode(json_data)
-        with open(file_path, "w") as fh:
-            fh.write(encoded)
-
-    def get_bson_data(self, bson_name):
-        import bson
-
-        file_path = self.get_config_dir_filename("bson", bson_name)
-        with open(file_path, "r") as fh:
-            raw = fh.read()
-        if len(raw) > 0:
-            bson_data = bson.loads(raw)
-            data = bson_data[bson_name]
-        else:
-            raise IOError("Blank BSON data")
-        return data
-
-    def save_bson_data(self, bson_name, data):
-        import bson
-
-        file_path = self.get_config_dir_filename("bson", bson_name)
-        bson_data = {bson_name: data}
-        raw = bson.dumps(bson_data)
-        with open(file_path, "w") as fh:
-            fh.write(raw)
 
     # class attributes
 
---
+++ omnivore/framework/persistence.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import json
+
+import jsonpickle
+from datetime import datetime
+
+from omnivore.third_party.appdirs import user_config_dir, user_log_dir, user_cache_dir, user_data_dir
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class FilePersistenceMixin(object):
+    def setup_file_persistence(self, app_name):
+        dirname = user_config_dir(app_name)
+        self.app_home_dir = dirname
+
+        # Make sure it exists!
+        if not os.path.exists(self.app_home_dir):
+            os.makedirs(self.app_home_dir)
+
+        dirname = user_log_dir(app_name)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        self.log_dir = dirname
+
+        self.log_file_ext = "-%s" % datetime.now().strftime("%Y%m%d-%H%M%S")
+
+        dirname = user_cache_dir(app_name)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        self.cache_dir = dirname
+
+        dirname = user_data_dir(app_name)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        self.user_data_dir = dirname
+
+        # Prevent py2exe from creating a dialog box on exit saying that there
+        # are error messages.  It thinks that anything written to stderr is an
+        # error, and the python logging module redirects everything to stderr.
+        # Instead, redirect stderr to a log file in the user log directory
+        frozen = getattr(sys, 'frozen', False)
+        if frozen in ('dll', 'windows_exe', 'console_exe'):
+            # redirect py2exe stderr/stdout to log file
+            log = self.get_log_file_name("py2exe")
+            oldlog = sys.stdout
+            sys.stdout = open(log, 'w')
+            if hasattr(oldlog, "saved_text"):
+                sys.stdout.write("".join(oldlog.saved_text))
+            sys.stderr = sys.stdout
+
+            # The logging module won't redirect to the new stderr without help
+            handler = logging.StreamHandler(sys.stderr)
+            logger = logging.getLogger('')
+            logger.addHandler(handler)
+        else:
+            log = self.get_log_file_name("log")
+            handler = logging.FileHandler(log)
+            formatter = logging.Formatter("%(levelname)s:%(name)s:%(msg)s")
+            handler.setFormatter(formatter)
+            logger = logging.getLogger('')
+            logger.addHandler(handler)
+
+    def get_log_file_name(self, log_file_name_base, ext=""):
+        filename = log_file_name_base + self.log_file_ext
+        if ext:
+            if not ext.startswith("."):
+                filename += "."
+            filename += ext
+        else:
+            filename += ".log"
+        filename = os.path.join(self.log_dir, filename)
+        return filename
+
+    def save_log(self, text, log_file_name_base, ext=""):
+        filename = self.get_log_file_name(log_file_name_base, ext)
+
+        try:
+            with open(filename, "wb") as fh:
+                fh.write(text)
+        except IOError:
+            log.error("Failed writing %s to %s" % (log_file_name_base, filename))
+
+    def get_config_dir_filename(self, subdir, json_name):
+        dirname = os.path.join(self.app_home_dir, subdir)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        return os.path.join(dirname, json_name)
+
+    def get_json_data(self, json_name, default_on_error=None):
+        try:
+            file_path = self.get_config_dir_filename("json", json_name)
+            with open(file_path, "r") as fh:
+                raw = fh.read()
+            json_data = jsonpickle.decode(raw)
+            try:
+                # new format is a list with a format identifier as the first ntry
+                if json_data[0] == "format=v2":
+                    decoded = json_data[1]
+            except KeyError:
+                # deprecated format was a dictionary, and was creating an xtra
+                # layer of indirection by encoding the jsonpickle as another tring
+                json_data = json.loads(raw)
+                encoded = json_data[json_name]
+                decoded = jsonpickle.decode(encoded)
+            return decoded
+        except IOError:
+            # file not found
+            return default_on_error
+        except ValueError:
+            # bad JSON format
+            log.error("Bad JSON format in preferences file: %s" % json_name)
+            return default_on_error
+
+    def save_json_data(self, json_name, data):
+        file_path = self.get_config_dir_filename("json", json_name)
+        json_data = ["format=v2", data]
+        encoded = jsonpickle.encode(json_data)
+        with open(file_path, "w") as fh:
+            fh.write(encoded)
+
+    def get_bson_data(self, bson_name):
+        import bson
+
+        file_path = self.get_config_dir_filename("bson", bson_name)
+        with open(file_path, "r") as fh:
+            raw = fh.read()
+        if len(raw) > 0:
+            bson_data = bson.loads(raw)
+            data = bson_data[bson_name]
+        else:
+            raise IOError("Blank BSON data")
+        return data
+
+    def save_bson_data(self, bson_name, data):
+        import bson
+
+        file_path = self.get_config_dir_filename("bson", bson_name)
+        bson_data = {bson_name: data}
+        raw = bson.dumps(bson_data)
+        with open(file_path, "w") as fh:
+            fh.write(raw)
