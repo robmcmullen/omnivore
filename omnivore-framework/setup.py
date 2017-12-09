ced5ff19b1b13d941d8110353cf1a97cb3c3413b
Rob McMullen <feedback@playermissile.com>
2017-12-08 22:39:03 -0800

    Updated omnivore-framework to rc12

--- omnivore-framework/setup.py
+++ omnivore-framework/setup.py
@@ -37,7 +37,7 @@ install_requires = [
 cmdclass = dict()
 
 import omnivore
-full_version = "1.0rc11"
+full_version = "1.0rc12"
 spaceless_version = full_version.replace(" ", "_")
 
 common_includes = [
