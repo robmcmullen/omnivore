ada7579cb8a0fd4b23d2770ae565bffd9ce56ffc
Rob McMullen <rob.mcmullen@noaa.gov>
2018-05-29 19:00:47 -0700

    2to3 on installdeps.py

--- installdeps.py
+++ installdeps.py
@@ -26,10 +26,10 @@ def git(args, branch=None):
 dry_run = False
 if dry_run:
     def dry_run_call(args):
-        print("in %s: %s" % (os.getcwd(), " ".join(args)))
+        print(("in %s: %s" % (os.getcwd(), " ".join(args))))
     subprocess.call = dry_run_call
     def dry_run_symlink(source, name):
-        print("in %s: %s -> %s" % (os.getcwd(), name, source))
+        print(("in %s: %s -> %s" % (os.getcwd(), name, source)))
     os.symlink = dry_run_symlink
 
 setup = "python setup.py "
@@ -48,9 +48,9 @@ for dep in deps:
     except ValueError:
         repourl = dep[0]
         options = {}
-    print dep, repourl, options
+    print(dep, repourl, options)
     if repourl.startswith("http") or repourl.startswith("git@"):
-        print "UPDATING %s" % repourl
+        print("UPDATING %s" % repourl)
         _, repo = os.path.split(repourl)
         repodir, _ = os.path.splitext(repo)
         if os.path.exists(repodir):
@@ -84,7 +84,7 @@ for dep in deps:
         os.chdir(linkdir)
         name = link_map.get(repodir, repodir)
         if name is None:
-            print("No link for %s" % repodir)
+            print(("No link for %s" % repodir))
         else:
             src = os.path.normpath(os.path.join("deps", repodir, builddir, name))
             if os.path.islink(name):
