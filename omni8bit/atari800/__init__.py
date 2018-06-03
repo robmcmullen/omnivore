11dada8d5d67c43ac6f0eb278f80bc8601566c10
Rob McMullen <feedback@playermissile.com>
2018-06-02 17:41:19 -0700

    Removed hard dependency on wx

--- omni8bit/atari800/__init__.py
+++ omni8bit/atari800/__init__.py
@@ -5,7 +5,11 @@ import numpy as np
 from . import libatari800 as liba8
 from . import generic_interface as g
 from . import akey
-from ..ui.screen import BitmapScreen, OpenGLScreen, GLSLScreen
 from .save_state_parser import parse_state
 from .colors import NTSC
 from .atari800 import Atari800, wxAtari800
+
+try:
+    from ..ui.screen import BitmapScreen, OpenGLScreen, GLSLScreen
+except ImportError:
+    pass
