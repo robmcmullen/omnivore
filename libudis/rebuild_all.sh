95b88284008197e1cf8e1965b6882f945dc7c8f5
Rob McMullen <feedback@playermissile.com>
2018-07-18 14:35:23 -0700

    libudis: fixed missing jumpman level disassembly

--- libudis/rebuild_all.sh
+++ libudis/rebuild_all.sh
@@ -2,7 +2,7 @@
 
 python cpugen.py
 #python disasm_gen.py -a
-#cython udis_fast/disasm_speedups.pyx
 python disasm_gen.py -m
-cython udis_fast/disasm_info.pyx udis_fast/disasm_speedups_monolithic.pyx
-python setup.py build_ext --inplace
+#cython disasm_speedups.pyx
+cython disasm_info.pyx disasm_speedups_monolithic.pyx
+#python setup.py build_ext --inplace
