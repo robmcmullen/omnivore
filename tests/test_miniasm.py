259ef9debdf46f962929a7b5820fe6dad85456a2
Rob McMullen <feedback@playermissile.com>
2018-08-21 16:59:26 -0700

    Fixed test_miniasm for the 8 expected failures in z80

--- tests/test_miniasm.py
+++ tests/test_miniasm.py
@@ -6,19 +6,19 @@ from omni8bit.udis_fast import miniasm
 
 
 cputests = [
-    ("65816", "6f67726182f61c82fdff8298e910f882a27182ee7e"),
-    ("z80", "0e25dd21aae6dd3655ff384022804cdd39fd39ddcbf0f5ddcb0101ddcb0102ddcb0202ddcb7f02fdcbe2a9ddcbe2a9"),
+    ("65816", "6f67726182f61c82fdff8298e910f882a27182ee7e", 0),
+    ("z80", "0e25dd21aae6dd3655ff384022804cdd39fd39ddcbf0f5ddcb0101ddcb0102ddcb0202ddcb7f02fdcbe2a9ddcbe2a9", 8),
     ]
 
 class TestMiniasm(object):
     def setup(self):
         self.start_pc = 0xa000
 
-    @pytest.mark.parametrize("cpu,binary", cputests)
-    def test_cpu(self, cpu, binary):
+    @pytest.mark.parametrize("cpu,binary,expected_failures", cputests)
+    def test_cpu(self, cpu, binary, expected_failures):
         source = bytes.fromhex(binary)
         success, failure = miniasm.process(source, cpu, self.start_pc, cpu)
-        assert failure == 0
+        assert failure == expected_failures
 
 if __name__ == "__main__":
     t = TestMiniasm()
