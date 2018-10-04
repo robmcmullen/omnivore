423fdaac4a2dc67b90ad852dbdea1610cb1a0eaa
Rob McMullen <feedback@playermissile.com>
2018-10-04 11:35:41 -0700

    Added atari800 history dtype and flags

--- omni8bit/disassembler/dtypes.py
+++ omni8bit/disassembler/dtypes.py
@@ -35,6 +35,29 @@ HISTORY_6502_DTYPE = np.dtype([
     ("extra2", np.uint8),
 ])
 
+HISTORY_ATARI800_DTYPE = np.dtype([
+    ("pc", np.uint16),
+    ("target_addr", np.uint16),
+    ("num_bytes", np.uint8),
+    ("disassembler_type", np.uint8),
+    ("flag", np.uint8),
+    ("cycles", np.uint8),
+    ("instruction", np.uint8, 3),
+    ("a", np.uint8),
+    ("x", np.uint8),
+    ("y", np.uint8),
+    ("sp", np.uint8),
+    ("sr", np.uint8),
+    ("before1", np.uint8),
+    ("after1", np.uint8),
+    ("before2", np.uint8),
+    ("after2", np.uint8),
+    ("before3", np.uint8),
+    ("after3", np.uint8),
+    ("antic_xpos", np.uint8),
+    ("antic_ypos", np.uint8),
+])
+
 HISTORY_FRAME_DTYPE = np.dtype([
     ("frame_number", np.uint32),
     ("num_bytes", np.uint8),
