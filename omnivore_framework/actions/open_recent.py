00b6ce7a1764a349f9870bbc78b7c61bb86648d3
Rob McMullen <feedback@playermissile.com>
2019-03-03 09:52:26 -0800

    Fixed open_recent to use OmnivoreListAction

--- omnivore_framework/actions/open_recent.py
+++ omnivore_framework/actions/open_recent.py
@@ -1,11 +1,9 @@
-from ..action import OmnivoreAction
+from ..action import OmnivoreListAction
 from .. import errors
 from ..persistence import get_json_data, save_json_data
 
-class open_recent(OmnivoreAction):
+class open_recent(OmnivoreListAction):
     prefix = "open_recent_"
-
-    prefix_count = len(prefix)
 
     max_entries = 20
 
@@ -28,32 +26,18 @@ class open_recent(OmnivoreAction):
             cls.canonical_list = cls.canonical_list[:cls.max_entries]
         save_json_data("recent_files", cls.canonical_list)
 
-    def init_from_editor(self):
+    def calc_list_items(self):
         if self.canonical_list is None:
             self.reload()
-        self.current_list = self.canonical_list[:]
-
-    def get_index(self, action_key):
-        return int(action_key[self.prefix_count:])
+        return self.canonical_list[:]
 
     def get_path(self, action_key):
         return self.current_list[self.get_index(action_key)]
 
-    def calc_name(self, action_key):
-        if len(self.current_list) == 0:
-            return "No recent files"
-        return self.get_path(action_key)
-
-    def calc_menu_sub_keys(self, action_key):
-        if len(self.current_list) == 0:
-            return [self.prefix + "empty"]
-        return [f"{self.prefix}{i}" for i in range(len(self.current_list))]
-
     def sync_menu_item_from_editor(self, action_key, menu_item):
         if self.current_list != self.canonical_list:
             raise errors.RecreateDynamicMenuBar
-        state = not len(self.current_list) == 0
-        menu_item.Enable(state)
+        OmnivoreListAction.sync_menu_item_from_editor(self, action_key, menu_item)
 
     def perform(self, action_key):
         path = self.get_path(action_key)
