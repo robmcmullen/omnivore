0c14ec5c7d5fc40fd0b2b4ac171e316e5fe3680d
Rob McMullen <feedback@playermissile.com>
2019-03-14 15:20:32 -0700

    Removed ext from fleep loaders since it may not recognize an extension and ext is not used anymore anyway

--- sawx/loaders/fleep.py
+++ sawx/loaders/fleep.py
@@ -8,6 +8,6 @@ def identify_mime(header, fh):
     info = fleep.get(header)
     if info.mime:
         log.debug(f"fleep loader: identified {info.mime}")
-        return dict(mime=info.mime, ext=info.ext)
+        return dict(mime=info.mime)
     log.debug(f"fleep loader: unidentified")
     return None
