545ae0881b42533eee772c62eb4c9f5275dfc71b
Rob McMullen <feedback@playermissile.com>
2019-05-19 20:56:09 -0700

    Added check to barf on zip matches (or any multiple matches) from fleep

--- sawx/loaders/fleep.py
+++ sawx/loaders/fleep.py
@@ -7,7 +7,11 @@ log = logging.getLogger(__name__)
 def identify_loader(file_guess):
     info = fleep.get(file_guess.sample_data)
     if info.mime:
-        log.debug(f"fleep loader: identified {info.mime}")
-        return dict(mime=info.mime)
+        if isinstance(info.mime, list):
+            log.debug(f"fleep loader: identified multiple mime types {info.mime}; skipping")
+            return None
+        else:
+            log.debug(f"fleep loader: identified {info.mime}")
+            return dict(mime=info.mime)
     log.debug(f"fleep loader: unidentified")
     return None
