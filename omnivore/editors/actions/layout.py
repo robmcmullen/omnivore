ab3fbb8d41c19dfafe6c99faef801604723b45ff
Rob McMullen <feedback@playermissile.com>
2019-07-02 19:15:35 -0700

    Added emulator layout save/restore

--- omnivore/editors/actions/layout.py
+++ omnivore/editors/actions/layout.py
@@ -6,26 +6,48 @@ from sawx.persistence import get_json_data, save_json_data
 class layout_save(SawxAction):
     name = "Save Layout"
 
+    def save_layout(self, label, layout):
+        layout_restore.add(label, layout)
+
     def perform(self, action_key):
         label = self.editor.frame.prompt("Enter name for layout", "Save Layout")
         if label:
             layout = {}
             self.editor.serialize_session(layout, False)
-            import pprint
-            pprint.pprint(layout)
-
-            layout_restore.add(label, layout)
+            self.save_layout(label, layout)
 
 
 class layout_restore(SawxPersistentDictAction):
     prefix = "layout_restore_"
 
-    json_name = "tilemanager_layout"
+    json_name = "tilemanager_layouts"
 
     def get_layout(self, action_key):
         label = self.current_list[self.get_index(action_key)]
-        return self.canonical_dict[label]
+        d = self.get_dict()
+        return d[label]
 
     def perform(self, action_key):
         layout = self.get_layout(action_key)
         self.editor.replace_layout(layout)
+
+
+class layout_save_emu(layout_save):
+    name = "Save Emulator Layout"
+
+    def save_layout(self, label, layout):
+        layout_restore_emu.add(label, layout)
+
+
+class layout_restore_emu(layout_restore):
+    prefix = "layout_restore_emu_"
+
+    json_name = "tilemanager_emulator_layouts"
+
+    def perform(self, action_key):
+        doc = self.editor.document
+        doc.pause_emulator()
+        try:
+            layout_restore.perform(self, action_key)
+        finally:
+            doc.resume_emulator()
