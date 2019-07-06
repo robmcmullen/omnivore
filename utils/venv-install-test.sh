f264d6a6c495c74aad6bb70b84efc72c7bcf9c13
Rob McMullen <feedback@playermissile.com>
2019-07-05 22:44:06 -0700

    Added virtual env creation script; made utils executable

---
+++ utils/venv-install-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VENV=/tmp/omnivore-install-test$$
+OMNIVORE=$HOME/src/refactor/omnivore
+
+/usr/bin/python3 -m venv $VENV
+source $VENV/bin/activate
+
+echo $PATH
+pip install numpy pathlib2
+# pip install pytest
+# pip install pathlib2
+pip install $OMNIVORE/dist/omnivore-2.0a7.tar.gz
+# cd $OMNIVORE
+# pip install .
+
+echo "activate with:"
+echo "source $VENV/bin/activate"
